6e50a0308c63b74c87c7ae83dd03f074ebc14409
kw.ho <hkw1831@macbook-pro.mynet>
2023-02-19 22:47:59 +0800

    init commit

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Command } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -16,66 +16,66 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		['n', 'l', 'w', 'd', 'a', 't'].forEach(t => {
+			this.addIcon(t);
+			this.addActionCommand(t);
+		})
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+	addIcon(t: string) {
+		var obsidian = require('obsidian');
+		obsidian.addIcon(`${t}-icon`, `<text stroke='#000' transform='matrix(2.79167 0 0 2.12663 -34.0417 -25.2084)' xml:space='preserve' text-anchor='start' font-family='monospace' font-size='24' y='44' x='19' stroke-width='0' fill='currentColor'>#${t}</text>`);
+	}
+
+	addActionCommand(t: string) {
+		this.addCommand({
+			id: `to-w${t}-n${t}`,
+			name: `To w${t} or n${t}`,
+			icon: `${t}-icon`,
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				console.log(editor.getSelection());
+				const cursor = editor.getCursor();
+				const lineNumber = editor.getCursor().line;
+				const line = editor.getLine(lineNumber);
+				const replacedLine = line.replace(`#wn `, `#w${t} `)
+				                         .replace(`#wl `, `#w${t} `)
+				                         .replace(`#ww `, `#w${t} `)
+										 .replace(`#wd `, `#w${t} `)
+										 .replace(`#wa `, `#w${t} `)
+										 .replace(`#wt `, `#w${t} `)
+										 .replace(`#w1 `, `#w${t} `)
+										 .replace(`#w2 `, `#w${t} `)
+										 .replace(`#w3 `, `#w${t} `)
+										 .replace(`#w4 `, `#w${t} `)
+										 .replace(`#w5 `, `#w${t} `)
+										 .replace(`#w6 `, `#w${t} `)
+										 .replace(`#w7 `, `#w${t} `)
+										 .replace(`#nn `, `#n${t} `)
+										 .replace(`#nl `, `#n${t} `)
+										 .replace(`#nw `, `#n${t} `)
+										 .replace(`#nd `, `#n${t} `)
+										 .replace(`#na `, `#n${t} `)
+										 .replace(`#nt `, `#n${t} `)
+										 .replace(`#n1 `, `#n${t} `)
+										 .replace(`#n2 `, `#n${t} `)
+										 .replace(`#n3 `, `#n${t} `)
+										 .replace(`#n4 `, `#n${t} `)
+										 .replace(`#n5 `, `#n${t} `)
+										 .replace(`#n6 `, `#n${t} `)
+										 .replace(`#n7 `, `#n${t} `);
+				editor.setLine(lineNumber, replacedLine);
+				editor.setCursor(cursor);
+			},
+			hotkeys: [
+				{
+					modifiers: [`Ctrl`, `Meta`, `Shift`],
+					key: `${t}`,
+				},
+			]
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -88,22 +88,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
