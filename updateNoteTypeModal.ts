764b7850e31c566597d54360bc5ee7c1eafcb526
kw.ho <hokawing1831@gmail.com>
2023-05-28 09:38:53 +0800

    updateNoteTypeModal use FuzzySuggestModal

--- updateNoteTypeModal.ts
+++ updateNoteTypeModal.ts
@@ -1,4 +1,4 @@
-import { App, Editor, Notice, SuggestModal } from "obsidian";
+import { App, Editor, FuzzySuggestModal, FuzzyMatch } from "obsidian";
 
 interface NoteType {
   type: string;
@@ -100,7 +100,7 @@ const ALL_TYPES = [
   },
 ];
 
-export class UpdateNoteTypeModal extends SuggestModal<NoteType> {
+export class UpdateNoteTypeModal extends FuzzySuggestModal<NoteType> {
 
   editor: Editor
 
@@ -109,15 +109,18 @@ export class UpdateNoteTypeModal extends SuggestModal<NoteType> {
     super(app)
     this.editor = editor
   }
-  // Returns all available suggestions.
-  getSuggestions(query: string): NoteType[] {
-    return ALL_TYPES.filter((noteType) =>
-        noteType.type.toLowerCase().includes(query.toLowerCase())
-    );
+
+  getItems(): NoteType[] {
+    return ALL_TYPES;
+  }
+
+  getItemText(noteType: NoteType): string {
+    return noteType.type;
   }
 
   // Renders each suggestion item.
-  renderSuggestion(noteType: NoteType, el: HTMLElement) {
+  renderSuggestion(choosenNoteTypeMatch: FuzzyMatch<NoteType>, el: HTMLElement) {
+    const noteType = choosenNoteTypeMatch.item
     el.createEl("div", { text: noteType.type });
     el.createEl("small", { text: noteType.description });
   }
@@ -127,13 +130,11 @@ export class UpdateNoteTypeModal extends SuggestModal<NoteType> {
   }
 
   // Perform action on the selected suggestion.
-  onChooseSuggestion(choosenNoteType: NoteType, evt: MouseEvent | KeyboardEvent) {
+  onChooseItem(choosenNoteType: NoteType, evt: MouseEvent | KeyboardEvent) {
     const selection = this.editor.getSelection()
     const replacedStr = `---\ntag: ${choosenNoteType.type}\n---\n\n`
     if (selection.length != 0) {
         this.editor.replaceSelection(replacedStr);
-        // cursor.ch = cursor.ch + replacedStr.length;
-        // this.editor.setCursor(cursor);
     } else {
         const cursor = this.editor.getCursor();
         const lineNumber = this.editor.getCursor().line;
