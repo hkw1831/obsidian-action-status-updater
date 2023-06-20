25a7e8d0e1640f9d334aa7c2387ac0eaff29fe2c
kw.ho <hkw1831@macbook-pro.mynet>
2023-06-20 11:50:22 +0800

    Add command to open inbox

--- main.ts
+++ main.ts
@@ -96,6 +96,20 @@ export default class MyPlugin extends Plugin {
 			},
 		})
 
+		this.openInboxIcon()
+		this.addCommand({
+			id: "open-inbox",
+			name: "Open Inbox",
+			icon: "open-inbox-icon",
+			callback: async () => {
+				const { vault } = this.app;
+				const { workspace } = this.app;
+				const inboxMd = "I/Inbox.md"
+				const mode = (this.app.vault as any).getConfig("defaultViewMode");
+				const leaf = workspace.getLeaf(false);
+				await leaf.openFile(vault.getAbstractFileByPath(inboxMd) as TFile, { active : true,/* mode */});
+			},
+		})
 
 		this.addAddCommentTagIcon();
 		this.addCommand({
@@ -196,6 +210,11 @@ export default class MyPlugin extends Plugin {
 	openDashboardIcon() {
 		var obsidian = require('obsidian');
 		obsidian.addIcon(`open-dashboard-icon`, `<text stroke='#000' transform='matrix(2.79167 0 0 2.12663 -34.0417 -25.2084)' xml:space='preserve' text-anchor='start' font-family='monospace' font-size='24' y='44' x='19' stroke-width='0' fill='currentColor'>OD</text>`);
+	}
+
+	openInboxIcon() {
+		var obsidian = require('obsidian');
+		obsidian.addIcon(`open-inbox-icon`, `<text stroke='#000' transform='matrix(2.79167 0 0 2.12663 -34.0417 -25.2084)' xml:space='preserve' text-anchor='start' font-family='monospace' font-size='24' y='44' x='19' stroke-width='0' fill='currentColor'>OI</text>`);
 	}
 
 	getQueryDateString(addDay: Number, excludeNote: String): string {
