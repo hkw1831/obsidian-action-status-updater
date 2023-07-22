0ae94e1cce3b72eaac0c20a08e3ded46d2502659
kw.ho <hkw1831@macbook-pro.mynet>
2023-07-22 18:16:17 +0800

    add url callback for threads to image calling ios shortcuts

--- main.ts
+++ main.ts
@@ -320,7 +320,7 @@ export default class MyPlugin extends Plugin {
 			
 				navigator.clipboard.writeText(text).then(function () {
 					new Notice(`Copy\n\`\`\`\n${text}\`\`\`\nto clipboard!`);
-					window.open('shortcuts://run-shortcut?name=Threads%20to%20image');
+					window.open('shortcuts://run-shortcut?name=Threads%20to%20image&x-success=obsidian://&x-cancel=obsidian://&x-error=obsidian://');
 				}, function (error) {
 					new Notice(`error when copy to clipboard!`);
 				});
