1c7b6993236db6ccfee2577989e5d9a7400de066
N <n@Ns-MacBook.local>
2023-08-27 01:12:38 +0800

    add more to the chatgpt prompt for generate twitter thread post

--- main.ts
+++ main.ts
@@ -466,7 +466,7 @@ export default class MyPlugin extends Plugin {
 				
 				let prompt = this.convertThreadsContentToFormatForFacebookApp(editor)
 				let numTweet = Math.ceil(prompt.length / 110)
-				prompt = `Convert the following content to twitter threads less than ${numTweet} tweet in traditional Chinese. Preserve the title. No need to add any tags to the tweet. Do not have any number in each tweet. Each tweet separated by newline character and 3 "-" characters and another newline character\n\n${prompt}`
+				prompt = `Convert the following content to twitter threads less than ${numTweet} tweet in traditional Chinese. Preserve the title. Do not add any additional information which is not mentioned from the original content. No need to add any tags to the tweet. Do not have any number in each tweet. Each tweet separated by newline character and 3 "-" characters and another newline character\n\n${prompt}`
 				prompt = prompt.replace(/▍/g, "")
 				prompt = prompt.replace(/】\n+https\:\/\/github.com[^\n]+\n/m, "】\n")
 
