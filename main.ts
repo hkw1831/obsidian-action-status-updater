dd552097ef599989ab333c7280393c4c20ef7ef5
N <n@Ns-MacBook.local>
2023-09-02 17:41:04 +0800

    add ne / we for expand content

--- main.ts
+++ main.ts
@@ -23,7 +23,7 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		['n', 'l', 'w', 'd', 'a', 't', 'm', '1', '2', '3', '4', '5', '6', '7'].forEach(t => {
+		['n', 'l', 'w', 'd', 'a', 't', 'm', 'e', '1', '2', '3', '4', '5', '6', '7'].forEach(t => {
 			this.addIcon(t);
 			this.addActionCommand(t);
 		});
