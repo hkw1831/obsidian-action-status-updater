9281342dea3b4b216b41fd92103f56ab99328d93
kw.ho <hkw1831@macbook-pro.mynet>
2023-12-05 11:49:34 +0800

    Update Count Action icon

--- main.ts
+++ main.ts
@@ -1267,7 +1267,7 @@ export default class MyPlugin extends Plugin {
 
 	addActionTagCountIcon() {
 		var obsidian = require('obsidian');
-		obsidian.addIcon(`action-tag-count-icon`, `<text stroke='#000' transform='matrix(2.79167 0 0 2.12663 -34.0417 -25.2084)' xml:space='preserve' text-anchor='start' font-family='monospace' font-size='24' y='44' x='19' stroke-width='0' fill='currentColor'>AC</text>`);
+		obsidian.addIcon(`action-tag-count-icon`, `<text stroke='#000' transform='matrix(2.79167 0 0 2.12663 -34.0417 -25.2084)' xml:space='preserve' text-anchor='start' font-family='monospace' font-size='24' y='44' x='19' stroke-width='0' fill='currentColor'>CA</text>`);
 	}
 
 	addGrepThreadsToClipboardIcon() {
