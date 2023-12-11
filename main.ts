eff7cb7ff200f7fb78792f0f80a46f826f3b967e
kw.ho <hkw1831@macbook-pro.mynet>
2023-12-11 12:34:38 +0800

    Update threads to twitter icon

--- main.ts
+++ main.ts
@@ -1424,7 +1424,7 @@ export default class MyPlugin extends Plugin {
 
 	addThreadsToTwitterIcon() {
 		var obsidian = require('obsidian');
-		obsidian.addIcon(`threads-to-twitter`, `<text stroke='#000' transform='matrix(2.79167 0 0 2.12663 -34.0417 -25.2084)' xml:space='preserve' text-anchor='start' font-family='monospace' font-size='24' y='44' x='19' stroke-width='0' fill='currentColor'>TT</text>`);
+		obsidian.addIcon(`threads-to-twitter`, `<text stroke='#000' transform='matrix(2.79167 0 0 2.12663 -34.0417 -25.2084)' xml:space='preserve' text-anchor='start' font-family='monospace' font-size='24' y='44' x='19' stroke-width='0' fill='currentColor'>TX</text>`);
 	}
 
 	addTwitterToChatGPTIcon() {
