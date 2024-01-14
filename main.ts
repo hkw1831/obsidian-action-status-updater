a4d068ffd240ea2d212e5a4853b6ba80694b912e
N <n@Ns-MacBook.local>
2024-01-14 23:27:34 +0800

    remove trailing newline for threads to clipboard

--- main.ts
+++ main.ts
@@ -1677,6 +1677,7 @@ export default class MyPlugin extends Plugin {
 
 		text2 = text2.replace("\n\n\n\n", "") // remove empty line
 		text2 = text2.replace("\nᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳᅳ\n\n\n", "") // remove empty line
+		text2 = text2.replace(/\n+$/, "")
 
 		return text2
 	}
