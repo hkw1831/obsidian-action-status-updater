f3e98169741136e9228875b7725e529ea28db807
N <n@Ns-MacBook.local>
2024-01-21 21:30:14 +0800

    set cursor when add text to selected note

--- selfutil/addlinktonotes.ts
+++ selfutil/addlinktonotes.ts
@@ -1,4 +1,4 @@
-import { App, Notice, TFile, TextFileView, Vault, Workspace } from "obsidian"
+import { App, Editor, MarkdownView, Notice, TFile, TextFileView, Vault, Workspace } from "obsidian"
 
 export function addTextToNotes(textToAdd: string, toPath: string, app: App, insertFromBeginning: boolean) {
     const vault: Vault = this.app.vault;
@@ -11,9 +11,11 @@ export function addTextToNotes(textToAdd: string, toPath: string, app: App, inse
         return leaf.openFile(tFile, { active: true });
     })
     .then(() => {
-        const editor = app.workspace.getActiveViewOfType(TextFileView);
-        const value = editor?.getViewData()
-        if (editor == null || value == null) {
+        //const editor = app.workspace.getActiveViewOfType(TextFileView);
+        const markdownView = app.workspace.getActiveViewOfType(MarkdownView);
+        const editor = markdownView?.editor
+        const value = markdownView?.getViewData()
+        if (markdownView == null || editor == null || value == null) {
             const errorReason = `editor or value ${toPath} not exist. Aborting...`
             return Promise.reject(errorReason)
         }
@@ -25,7 +27,17 @@ export function addTextToNotes(textToAdd: string, toPath: string, app: App, inse
             const newValue = insertFromBeginning ? 
             getNoteValueInsertingTextFromStartOfNotes(value, link) : 
             getNoteValueInsertingTextFromEndOfNotes(value, link)
-            editor.setViewData(newValue, false)
+            markdownView.setViewData(newValue, false)
+            if (insertFromBeginning) {
+                const frontMatterRegex = /^(---\n[\s\S]*?\n---\n)/gm
+                if (frontMatterRegex.test(value)) {
+                    editor.setCursor({ line: getLineAfterFrontMatter(value), ch: 0 }) 
+                } else {
+                    editor.setCursor({ line: 0, ch: 0 })
+                }
+            } else {
+                editor.setCursor({ line: editor.lineCount() - 1, ch: 0 })
+            }
             new Notice(`Added link to ${insertFromBeginning ? "beginning" : "end"} of ${toPath}!`);
         }
         return Promise.resolve()
@@ -58,6 +70,21 @@ export function addTextToNotes(textToAdd: string, toPath: string, app: App, inse
             return leaf.openFile(tFile, { active: true });
         })
         */
+}
+
+function getLineAfterFrontMatter(value: string) {
+    const values = value.split("\n")
+    let fmCount = 0
+    const lineNum = values.length
+    for (let i = 0; i < lineNum; i++) {
+        if (values[i] === "---") {
+            fmCount++
+        }
+        if (fmCount == 2) {
+            return Math.min(i + 1, lineNum - 1)
+        }
+    }
+    return lineNum - 1
 }
 
 function getNoteValueInsertingTextFromStartOfNotes(value: string, text: string) {
