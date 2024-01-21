dc01e6f62520ffa6135379de4b8bf84801ce409e
N <n@Ns-MacBook.local>
2024-01-21 11:12:08 +0800

    replace 3 consec newline to 2 newline

--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ import moment from 'moment';
 import { AddTextToNotesModal } from 'addTextToNotesModal';
 import { NavigateToNoteFromTagModal } from 'navigateToNoteFromTagModal';
 import { SelectionRange, exportCurrentSelection, getCurrentSelectionLineNumber } from 'selfutil/extractSelection';
-import { replaceTWUselessValue } from 'twmigration/twMigrateTools';
+import { getParentLine, replaceTWUselessValue, shouldSkipFrontMatter, tidyUpFrontMatterOnValue, tidyUpFrontMatteronEditor } from 'twmigration/twMigrateTools';
 
 // Remember to rename these classes and interfaces!
 
@@ -28,75 +28,6 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 }
 
 const clipboardHistory: string[] = []
-
-const skipFrontMatterField: string[] = [
-	"freetimetask: ",
-	"expectedtime: ",
-	"mode: ",
-	"days: ",
-	"showheaderfooter: ",
-	"showstate: ",
-	"sortsubpagefilter: ",
-	"throughttree: ",
-	"urgent: ",
-	"displayas: ",
-	"startdate: ",
-	"readwritemode: ",
-	"showChandlerNow: ",
-	"deepwork: ",
-	"expectedtime: ",
-	"parsedate: ",
-	"tidscope: ",
-	"inserttodoaction: ",
-	"optional: ",
-	"replaceto: ",
-	"backuptiddler: ",
-	"deadline: ",
-	"caption: ",
-	"collection: ",
-	"library: ",
-	"library_version: ",
-	"dummy: ",
-	"tidName: ",
-	"chronicledate: ",
-	"eventdate: ",
-	"dailyhighlight: ",
-	"displaycardmode: ",
-	"displaymode: ",
-	"numcol: ",
-	"dateyyyymmdd: ",
-	"graphdisplaymode: ",
-	"maxdepth: ",
-	"journaldate: ",
-	"thisBillDate: ",
-	"lastBillDate: ",
-	"roottiddler: ",
-	"year: ",
-	"to: ",
-	"tidtemplate: ",
-	"tiddlername: ",
-	"theme: ",
-	"tagvalue: ",
-	"subtasknum: ",
-	"removetagvalue: ",
-	"recurringeventstartdate: ",
-	"recurringeventenddate: ",
-	"pluginname: ",
-	"pid: ",
-	"macroname: ",
-	"limit: ",
-	"keyword: ",
-	"keywordtmp: ",
-	"from: ",
-	"deprecatereason: ",
-	"deepwo: ",
-	"color: ",
-	"row: ",
-	"col: ",
-	"bookmarked: ",
-	"blockingreminderdate: ",
-	"backup: "
-]
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -486,7 +417,7 @@ export default class MyPlugin extends Plugin {
 					// note that still async
 					console.log("s: " + startCount)
 					vault.read(file).then((content) => {
-						const modifiedValue = this.tidyUpFrontMatterOnValue(content)
+						const modifiedValue = tidyUpFrontMatterOnValue(content)
 						return vault.modify(file, modifiedValue);
 					}).then(() => {
 						console.log("f: " + finishedCount)
@@ -518,7 +449,7 @@ export default class MyPlugin extends Plugin {
 			name: "FN Format Notes (Custom usage)",
 			icon: `format-notes-custom`,
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				this.tidyUpFrontMatteronEditor(editor)
+				tidyUpFrontMatteronEditor(editor)
 			},
 			hotkeys: [
 				{
@@ -645,7 +576,7 @@ this.addCommand({
 		text += c
 		text = text.replace(/^---\n---\n/m, "").replace(/\n$/, "")
 		editor.setValue(text)
-		editor.setCursor({line: this.getParentLine(text), ch: 0})
+		editor.setCursor({line: getParentLine(text), ch: 0})
 	},
 	hotkeys: [
 		{
@@ -762,7 +693,7 @@ this.addCommand({
 					}
 					text = text.replace(/\n$/m, "")
 					editor.setValue(text)
-					editor.setCursor({line: this.getParentLine(text), ch: 0})
+					editor.setCursor({line: getParentLine(text), ch: 0})
 				} else {
 					editor.setValue(replaceTWUselessValue(editor.getValue()))
 					let text = "- "
@@ -782,7 +713,7 @@ this.addCommand({
 								content += "\n" + modifiedLine
 							}
 						} else if (h3Count == 1) {
-							if (line === "---" || this.shouldSkipFrontMatter(line) || line.startsWith("title: ")) {
+							if (line === "---" || shouldSkipFrontMatter(line) || line.startsWith("title: ")) {
 								// do nothing
 							} else if (line.startsWith("tagsss: ")) {
 								if (/ N /.test(line) || / N$/.test(line)) {
@@ -865,7 +796,7 @@ this.addCommand({
 					} 
 					text += content
 					editor.setValue(text)
-					editor.setCursor({line: this.getParentLine(text), ch: 0})
+					editor.setCursor({line: getParentLine(text), ch: 0})
 				}
 			},
 			hotkeys: [
@@ -1973,17 +1904,6 @@ this.addCommand({
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
 
-	getParentLine(value: string) {
-		const values = value.split("\n")
-		for (let i = 0; i < values.length; i++) {
-			const lineContent = values[i]
-			if (/^parent\d+: /.test(lineContent) || /^\t+- parent\d+: /.test(lineContent)) {
-				return i
-			}
-		}
-		return 0
-	}
-
 	convertChatGPTToTwitterFormat(editor: Editor) : boolean { // true means success
 		let line = editor.lineCount();
 
@@ -2132,113 +2052,6 @@ this.addCommand({
 		editor.setValue(text);
 	}
 */
-	tidyUpFrontMatteronEditor(editor: Editor) {
-		const value = editor.getValue()
-		const modifiedValue = this.tidyUpFrontMatterOnValue(value)
-		editor.setValue(modifiedValue)
-	}
-
-	tidyUpFrontMatterOnValue(value: String) {
-		const values: string[] = value.split("\n")
-		const lineCount = values.length
-
-		let fm = ""
-		let c = ""
-		let text = ""
-		let h3Count = 0;
-		let content = ""
-		for (let i = 0; i < lineCount; i++) {
-			const line = values[i]
-			if (h3Count == 0) {
-				content += (line + "\n")
-			} else if (h3Count == 1) {
-				if (this.shouldSkipFrontMatter(line)) {
-					// do nothing
-				} else if (line.startsWith("title: ")) {
-					const modifiedLine = line.replace(/:/g, "_").replace(/^title_ /, "title: ")
-					fm += (modifiedLine + "\n")
-				} else if (line === "tags: [excalidraw]"){
-					fm += (line + "\n")
-				} else if (line.startsWith("tagsss: ") || line.startsWith("tags: ")) {
-					const bracketPattern = /\[\[.*?\]\]/g;
-
-					// Find all bracketed items
-					const bracketedItems = line.match(bracketPattern) || [];
-					
-					// Remove bracketed items from the input string to deal with the remaining
-					const remainingString = line.replace(bracketPattern, '').trim();
-					
-					// Split the remaining string by spaces to get the individual words
-					const remainingItems = remainingString.split(/\s+/).filter(item => item);
-					
-					// Combine the bracketed items and the individual words into one array
-					const fmtagsss = [...bracketedItems, ...remainingItems];
-
-					let parent : string[] = []
-					let tagsss : string[] = []
-					let skips : string[]= []
-					
-					fmtagsss.forEach(tag => {
-						tag = tag.trim()
-						// [[event n]] / [[event w]] / regex of [[20220717 Journal (Week 28 Sun)]]: put in skips
-						// [[20220721 Journal (Week 29 Thu)]]
-						if (tag === "[[.Header Shortcut]]" || tag === "[[.Current Project]]" || tag === "concept" || tag === "space" || tag === "problem" || tag === "tagsss:" || tag === "tags:" || tag === "[[event n]]" || tag === "[[event w]]"
-							|| /\[\[\d{8} Journal \(Week \d+ [A-Za-z]{3}\)\]\]/.test(tag)) {
-							skips.push(tag)
-						} else if (tag === "permtask" || 	tag === "N" || tag === "W" || tag === "now" || tag === "later" || tag === "waiting" || tag === "done" || tag === "archive" || tag === "action" || tag === "task") {
-							tagsss.push(tag.replace("[[", "").replace("]]", ""))
-						} else if (tag === "preblog" || tag === "prepreblog") {
-							parent.push("[[Blog _ Post]]")
-						} else {
-							parent.push(tag)
-						}
-					})
-					//new Notice(skips.join("\n"))
-					let modifiedLine = ""
-					if (tagsss.length > 0) {
-						modifiedLine += "tagsss: " + tagsss.join(" ") + "\n"
-					}
-					let parentCount = 1
-					if (parent.length > 0) {
-						const uniqueParent = Array.from(new Set(parent));
-						uniqueParent.forEach(p => {
-							if (p.startsWith("[[") && p.endsWith("]]")) {
-								modifiedLine += "parent" + parentCount + ": \"" + p.replace(":", "_") + "\"\n"
-							} else {
-								modifiedLine += "parent" + parentCount + ": \"[[" + p.replace(":", "_") + "]]\"\n"
-							}
-							parentCount++
-						})
-					}
-					fm += modifiedLine
-				} else {
-					fm += (line + "\n")
-				}
-			}
-			if (h3Count >= 2) {
-				c += (line + "\n")
-			}
-			if (line === "---") {
-				h3Count++;
-			}
-		} 
-		text += content
-		if (fm.length > 0) {
-			text += fm
-		}
-		text += c
-		
-		return text.replace(/^---\n---\n/m, "").replace(/\n$/, "")
-	}
-
-	shouldSkipFrontMatter(line: string) : boolean {
-		for (let i = 0; i < skipFrontMatterField.length; i++) {
-			if (line.startsWith(skipFrontMatterField[i])) {
-				return true
-			}
-		}
-		return false
-	}
 
 	convertThreadsContentToLightPostFormat(editor: Editor, headerIcon: string, paragraphSeparator: string
 		, additionReplaceFn: (a: string) => string = (a) => a) : string {
