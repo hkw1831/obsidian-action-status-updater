2eba9d9790c638cea3ec7dbdfd47449add716c8c
N <n@Ns-MacBook.local>
2024-08-03 15:37:26 +0800

    add keyboard select item in multiple clipboard

--- clipboardPasteModal.ts
+++ clipboardPasteModal.ts
@@ -6,12 +6,42 @@ export class ClipboardPasteModal extends FuzzySuggestModal<string> {
 
   clipboardContent: string[]
 
+  keydownHandler: (event: KeyboardEvent) => void;
+
   constructor(app: App, editor: Editor, clipboardContent: string[])
   {
     super(app)
     this.editor = editor
     this.clipboardContent = clipboardContent
     this.setPlaceholder(`Which clipboard content do you want to paste?`)
+    this.keydownHandler = (event: KeyboardEvent) => {
+      if (event.metaKey && event.shiftKey && event.key === 'V') { // macos
+        this.close();
+      } else if (event.metaKey || event.ctrlKey) {
+        const key = parseInt(event.key, 10);
+        if (key >= 1 && key <= 9) {
+          event.preventDefault(); // Prevent default action
+          this.selectElement(key - 1); // Select the element (index key - 1)
+        }
+      }
+    };
+
+    // Listen for keydown events at the document level
+    document.addEventListener('keydown', this.keydownHandler);
+  }
+
+  onClose() {
+    super.onClose();
+    // Stop listening for keydown events when the modal is closed
+    document.removeEventListener('keydown', this.keydownHandler);
+  }
+
+  selectElement(index: number) {
+    const elements = this.resultContainerEl.querySelectorAll('.suggestion-item');
+    if (elements.length > index) {
+      const element = elements[index] as HTMLElement;
+      element.click(); // Simulate a click to select the element
+    }
   }
 
   getItems(): string[] {
@@ -26,7 +56,9 @@ export class ClipboardPasteModal extends FuzzySuggestModal<string> {
   renderSuggestion(i: FuzzyMatch<string>, el: HTMLElement) {
     const item = i.item
     //el.createEl("div", { text: item });
-    el.createEl("div", { text: "• " + item.replace(/\n/gm, "").substring(0, 100) });
+    const index = this.resultContainerEl.querySelectorAll('.suggestion-item').length;
+    const itemIndex = index < 10 ? index + ". " : "    "
+    el.createEl("div", { text: itemIndex + item.replace(/\n/gm, "").substring(0, 100) });
   }
 
   // Perform action on the selected suggestion.
