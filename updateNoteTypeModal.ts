0a9005daf516dbc04eb88a3764da5853c37a7c74
N <n@Ns-MacBook.local>
2024-08-03 15:49:59 +0800

    add keyboard select item in update tag typte modal

--- updateNoteTypeModal.ts
+++ updateNoteTypeModal.ts
@@ -6,12 +6,44 @@ export class UpdateNoteTypeModal extends FuzzySuggestModal<NoteType> {
 
   editor: Editor
   file: TFile
+  keydownHandler: (event: KeyboardEvent) => void;
 
   constructor(app: App, editor: Editor, file: TFile)
   {
     super(app)
     this.editor = editor
     this.file = file
+    this.keydownHandler = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.altKey && event.shiftKey && event.key === 'C') { // windows
+        this.close();
+      } else if (event.ctrlKey && event.metaKey && event.shiftKey && event.key === 'C') { // macos
+        this.close();
+      } else if (event.metaKey || event.ctrlKey) {
+        const key = parseInt(event.key, 10);
+        if (key >= 1 && key <= 9) {
+          event.preventDefault(); // Prevent default action
+          this.selectElement(key - 1); // Select the element (index key - 1)
+        }
+      }
+    };
+
+    // Listen for keydown events at the document level
+    document.addEventListener('keydown', this.keydownHandler);
+  }
+
+  selectElement(index: number) {
+    const elements = this.resultContainerEl.querySelectorAll('.suggestion-item');
+    if (elements.length > index) {
+      const element = elements[index] as HTMLElement;
+      element.click(); // Simulate a click to select the element
+    }
+  }
+
+  onClose() {
+    super.onClose();
+    // Stop listening for keydown events when the modal is closed
+    document.removeEventListener('keydown', this.keydownHandler);
+    //this.scope.unregister(); // Unregister the scope when the modal is closed
   }
 
   getItems(): NoteType[] {
@@ -25,8 +57,10 @@ export class UpdateNoteTypeModal extends FuzzySuggestModal<NoteType> {
   // Renders each suggestion item.
   renderSuggestion(choosenNoteTypeMatch: FuzzyMatch<NoteType>, el: HTMLElement) {
     const noteType = choosenNoteTypeMatch.item
-    el.createEl("div", { text: noteType.type });
-    el.createEl("small", { text: noteType.description });
+    const index = this.resultContainerEl.querySelectorAll('.suggestion-item').length;
+    const itemIndex = index < 10 ? index + ". " : "    "
+    el.createEl("div", { text: itemIndex + noteType.type });
+    el.createEl("small", { text: "     " + noteType.description });
   }
 
   containsType(line: String) : Boolean {
